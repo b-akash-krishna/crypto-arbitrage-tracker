731f1a56a20685141a8d59417c6cd12a0c402584
B AKASH KRISHNA <akash199699@gmail.com>
2025-11-20 01:07:59 +0530

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,6 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://crypto-arbitrage-tracker-alpha.vercel.app",
+        "https://*.vercel.app",
         "http://127.0.0.1:3000",
     ],
     allow_credentials=True,
